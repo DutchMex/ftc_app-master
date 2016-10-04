543b7f616ac37d618d1d7916f39f3ec2059df58b
Grant Brinkman <brinkman@brinkman-macbook.local>
2016-10-03 17:08:26 -0700

    test commit of program 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_1.java
@@ -1,7 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**
  * Created by brinkman on 10/3/16.
  */
 public class test_1 {
+
+    DcMotor LF;
+    DcMotor LB;
+    DcMotor RF;
+    DcMotor RB;
+    float LFP;
+    float LBP;
+    float RFP;
+    float RBP;
 }
