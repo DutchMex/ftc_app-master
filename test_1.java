a736ed8bb1fb3f6b360f95c2da4152d8e7b986aa
DutchMexican1120 <gminatorbrinkman@gmail.com>
2016-10-03 17:28:26 -0700

    Update test_1.java
    lglkufljgltdi

--- test_1.java
+++ test_1.java
@@ -17,12 +17,5 @@ public class test_1 {
     float RBP;
     @Override
     public void init() {
-        LF = hardwareMap.dcMotor.get("LF");
-        LB = hardwareMap.dcMotor.get("LB");
-        RF = hardwareMap.dcMotor.get("RF");
-        RB = hardwareMap.dcMotor.get("RB");
-
-        LF.setDirection(DcMotor.Direction.REVERSE);
-        LB.setDirection(DcMotor.Direction.REVERSE);
-
+       
     }
