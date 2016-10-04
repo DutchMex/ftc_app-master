b989acbdb9c01e1d69fec60f616601f7e7c14261
DutchMexican1120 <gminatorbrinkman@gmail.com>
2016-10-03 17:11:16 -0700

    test 3.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_1.java
@@ -24,5 +24,15 @@ public class test_1 {
 
         LF.setDirection(DcMotor.Direction.REVERSE);
         LB.setDirection(DcMotor.Direction.REVERSE);
+        @Override
+        public void init() {
+            LF = hardwareMap.dcMotor.get("LF");
+            LB = hardwareMap.dcMotor.get("LB");
+            RF = hardwareMap.dcMotor.get("RF");
+            RB = hardwareMap.dcMotor.get("RB");
 
-    }
+            LF.setDirection(DcMotor.Direction.REVERSE);
+            LB.setDirection(DcMotor.Direction.REVERSE);
+
+
+        }
